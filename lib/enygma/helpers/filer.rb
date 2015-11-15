729b3585ce10ac8697b926a140c294e44703624e
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2015-11-15 23:40:32 +0100

    Corrected error in the Filer class

--- lib/enygma/helpers/filer.rb
+++ lib/enygma/helpers/filer.rb
@@ -10,9 +10,9 @@ module Enygma
 			unless dest_file
 				name_split_array = source_file.split('.')
 
-				name_split_array = name_split_array.delete("encrypted")
-				name_split_array = name_split_array.delete("decrypted")
-				name_split_array = name_split_array.delete("cracked")
+				name_split_array.delete("encrypted")
+				name_split_array.delete("decrypted")
+				name_split_array.delete("cracked")
 
 				dest_file = name_split_array.insert(-2, action).join('.') # filename.encrypted.txt
 			end
